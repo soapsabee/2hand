64ed8b1e8b40ff2d7c0aec70edf253ae74af3244
xxx <xxxx>
2018-05-09 22:52:47 +0700

    เพิ่มให้มันเก็บเวลา Loginแล้ว

--- checklogin.php
+++ checklogin.php
@@ -1,7 +1,10 @@
 <?php
     session_start();
     include('connectdb.php');
+    date_default_timezone_set('Asia/Bangkok');
+    $time = date("H:i:s");
     $sql = 'SELECT * FROM user WHERE username = "'.$_POST['username'].'" AND password = "'.$_POST['password'].'"';
+    $logsql = 'INSERT INTO login_time(Log_id, Log_time, Log_date, username) VALUES (NULL,"'.$time.'",CURDATE(),"'.$_POST['username'].'")';
     $query = mysqli_query($connect,$sql);
     $result = mysqli_num_rows($query);
     $check = mysqli_fetch_array($query,MYSQLI_ASSOC);
@@ -11,10 +14,11 @@
     else if($check['Status']=='User'){
        echo "you can login";
        $_SESSION['username'] = $_POST['username'];
-       
+       $logquery = mysqli_query($connect,$logsql);
     }else{
         echo "Welcome Admin";
         $_SESSION['username'] = $_POST['username'];
+        $logquery = mysqli_query($connect,$logsql);
     }
     
     mysqli_close($connect);   
---
+++ testdate.php
@@ -0,0 +1,5 @@
+<?php
+date_default_timezone_set('Asia/Bangkok');
+$time = date("H:i:s");
+echo $time;
+?>
